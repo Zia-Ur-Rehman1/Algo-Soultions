0b9bcafe27f20ced92a15c35b29a211f66173836
Zia Ur Rehman <ziakhanalone100@gmail.com>
2024-09-22 01:01:15 +0500

    Create solutuion.rb

---
+++ Roman to Integer/solutuion.rb
@@ -0,0 +1,24 @@
+def roman_to_int(s)
+  hash ={
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }    
+  length = s.size
+  sum = 0
+  i=0
+  s= s.split('')
+  while i < length
+    if hash[s[i+1]&.to_sym] && hash[s[i].to_sym] < hash[s[i+1]&.to_sym]
+      sum -= hash[s[i].to_sym] 
+    else
+      sum += hash[s[i].to_sym]
+    end
+    i +=1
+  end
+  sum
+end
